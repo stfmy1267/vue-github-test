902d9493130489cce163382cb43753e98e89e93f
Satou Fumiya <c119115@g.koeki-u.ac.jp>
2022-08-09 17:35:15 +0900

    fix router base

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: (process.env.NODE_ENV === 'poduction')
-    ? '/vue-github-test/' : './',
-    resolve: {
-      // @/ によるimportをできるようにする。
-      alias: {
-        '@': path.resolve('__dirname', '/src')
-      }
-    },
+  base: '/vue-github-test/',
+  // base: (process.env.NODE_ENV === 'poduction') ? '/vue-github-test/' : './',
+  resolve: {
+    // @/ によるimportをできるようにする。
+    alias: {
+      '@': path.resolve('__dirname', '/src')
+    }
+  },
   build: {
     outDir: 'docs'
   },
