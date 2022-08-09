8c145ec5a8bd583feb4737786e492d439e92cc84
Satou Fumiya <c119115@g.koeki-u.ac.jp>
2022-08-09 17:26:36 +0900

    fix router base

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
-import Vue from 'vue';
-import * as VueRouter from 'vue-router';
+// import Vue from 'vue';
+// import * as VueRouter from 'vue-router';
 import { createRouter, createWebHistory } from 'vue-router'
 // HomeビューとAboutビューのインポート
 import Home from './views/Home.vue';
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: (process.env.NODE_ENV === 'poduction')
     ? '/vue-github-test/' : './',
+    resolve: {
+      // @/ によるimportをできるようにする。
+      alias: {
+        '@': path.resolve('__dirname', '/src')
+      }
+    },
   build: {
     outDir: 'docs'
   },
