de7765dd722ab97b81eddbd71452f0a110442f43
Satou Fumiya <c119115@g.koeki-u.ac.jp>
2022-08-09 13:24:45 +0900

    github pages test

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: (process.env.NODE_ENV === 'poduction')
+    ? '/vue-github-test/' : './',
+  build: {
+    outDir: 'docs'
+  },
   plugins: [vue()]
 })
